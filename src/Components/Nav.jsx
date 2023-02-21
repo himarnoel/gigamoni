14ee222d701e49385f6d7d193adf208c71b79a69
himarnoel <olaimarnoel@gmail.com>
2023-02-21 16:16:52 +0100

    adjust nav bar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,13 +3,19 @@ import logo from "../assets/logo.svg";
 const Nav = () => {
   return (
     <div className=" ">
-      <div className="w-full bg-[#87ACA3]/[0.3] px-10 absolute top-40 py-4 z-40 rounded-b-lg flex justify-between items-center">
-        <img src={logo} alt="" className="" />
-        <span className="flex text-white">
+      <div className="w-full bg-[#87ACA3]/[0.3] px-16 absolute top-40 py-3  z-40 rounded-b-lg flex justify-between items-center">
+        <a href="">
+          {" "}
+          <img src={logo} alt="" className="object-contain w-[9rem]" />
+        </a>
+        <span className="flex text-white w-[29rem] justify-between items-center">
           <a href="">How it works</a>
           <a href="">FaQs</a>
           <a href="">Login</a>
-          <button className="p-5 rounded bg[#009186]"> Create Account</button>
+          <button className="px-8 py-[0.7rem] rounded bg-[#009186]">
+            {" "}
+            Create Account
+          </button>
         </span>
       </div>
     </div>
