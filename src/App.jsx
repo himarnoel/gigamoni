366f73b5daae499ea42b1dbf5b1cd10bae7e5376
himarnoel <olaimarnoel@gmail.com>
2023-02-21 15:03:58 +0100

    added new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Nav from './Components/Nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
+    <div className="">
+      <Nav/>
     </div>
   )
 }
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import heroImage from '../assets/'
+const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav
