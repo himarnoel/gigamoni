950e46c24f66f2826a853ad23916526eccc0ad0c
himarnoel <olaimarnoel@gmail.com>
2023-02-21 16:07:17 +0100

    added button

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,11 +5,11 @@ const Nav = () => {
     <div className=" ">
       <div className="w-full bg-[#87ACA3]/[0.3] px-10 absolute top-40 py-4 z-40 rounded-b-lg flex justify-between items-center">
         <img src={logo} alt="" className="" />
-        <span className="flex">
+        <span className="flex text-white">
           <a href="">How it works</a>
           <a href="">FaQs</a>
           <a href="">Login</a>
-          <button className="p-54"></button>
+          <button className="p-5 rounded bg[#009186]"> Create Account</button>
         </span>
       </div>
     </div>
