9e2aad5e73f343f922e337ad8a1d5834a213d2ec
himarnoel <olaimarnoel@gmail.com>
2023-02-21 16:27:54 +0100

    edting the font style

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,11 +3,11 @@ import logo from "../assets/logo.svg";
 const Nav = () => {
   return (
     <div className=" ">
-      <div className="w-full bg-[#87ACA3]/[0.3] px-16 absolute top-40 py-3  z-40 rounded-b-lg flex justify-between items-center">
+      <div className="font-poppins w-full bg-[#87ACA3]/[0.3] px-16 absolute top-40 py-3  z-40 rounded-b-lg flex justify-between items-center">
         <a href="">
           <img src={logo} alt="" className="object-contain w-[9rem]" />
         </a>
-        <span className="flex text-white w-[29rem] justify-between items-center">
+        <span className="flex text-white w-[29rem] justify-between items-center font-[600] text-sm">
           <a href="">How it works</a>
           <a href="">FaQs</a>
           <a href="">Login</a>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
-}
+};
