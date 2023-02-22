096e65c4e71e0f0d83ab3d0948451e24d5977423
himarnoel <olaimarnoel@gmail.com>
2023-02-22 10:39:57 +0100

    almost done with responsiveness

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,10 +10,10 @@ const Hero = () => {
       <div className="">
         {/* global text */}
         <div
-          className="mx-2 text-white z-40 text-2xl left-0 right-0 md:right-[50rem]   md:text-left sm:text-4xl   leading-loose absolute top-24  lg:top-72 md:top-[8rem] md:left-8  lg:left-10 xl:left-24
+          className="mx-2 md:mx-1 text-white z-40 text-2xl left-0 right-0  md:right-[16rem]  lg:right-[27rem] xl:right-[50rem]   md:text-left sm:text-4xl   leading-loose absolute top-24  lg:top-[16rem] md:top-[10rem] md:left-8  lg:left-10 xl:left-24
   "
         >
-          <div className="">
+          <div className="leading-[2]">
             <p> Transfer globally, finance </p>
             <div className="flex items-center">
               <span className="relative  sm:w-[20rem] flex">
@@ -32,16 +32,16 @@ const Hero = () => {
             family, friends, and business partners faster and easier{" "}
           </p>
 
-          <ol className="md:text-sm ml-4 mt-8 text-sm  font-light list-disc leading-[2.3]">
+          <ol className="md:text-sm ml-4 md:mt-6 lg:mt-3 text-sm  font-light list-disc leading-[2.3]">
             <li>Transfer money online in African currencies</li>
             <li>Payment received in USD, GBP, and EUR </li>
           </ol>
           <img
             src={card}
             alt=""
-            className="mx-auto  md:hidden    mt-10    object-contain   w-[16rem]  xl:w-[23rem]  md:w-[15rem] lg:w-[20rem] lg:right-10 md:right-8 md:top-64    xl:right-24 z-[20]  lg:top-[24rem] xl:top-[20rem] "
+            className="mx-auto  md:hidden    mt-10 lg:mt-5   object-contain   w-[16rem]  xl:w-[23rem]  md:w-[15rem] lg:w-[20rem] lg:right-10 md:right-8 md:top-64    xl:right-24 z-[20]  lg:top-[2rem] xl:top-[20rem] "
           />
-          <button className="w-full md:w-fit  mt-5  px-8 py-[0.7rem] font-bold  text-sm rounded-lg bg-[#87ACA3] md:mt-2 text-[#262626]">
+          <button className="w-full md:w-fit  mt-5  px-8 py-[0.7rem] font-bold  text-sm rounded-lg bg-[#87ACA3] md:mt-8 lg:mt-3 text-[#262626]">
             Send Money
           </button>
         </div>
@@ -50,7 +50,7 @@ const Hero = () => {
             <img
               src={card}
               alt=""
-              className="absolute object-contain hidden md:block   w-[16rem]  xl:w-[23rem]  md:w-[15rem] lg:w-[20rem] lg:right-10 md:right-8 md:top-64    xl:right-24 z-[200]  lg:top-[24rem] xl:top-[18rem] "
+              className="absolute object-contain hidden md:block   w-[16rem]  xl:w-[23rem]  md:w-[15rem] lg:w-[20rem] lg:right-10 md:right-8 md:top-64    xl:right-24 z-[200]  lg:top-[19rem] xl:top-[18rem] "
             />
         </div>
       </div>
