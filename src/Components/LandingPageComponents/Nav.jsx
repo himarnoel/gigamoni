e07e0a16ce88e945993836d8f1be404e4937b35e
himarnoel <olaimarnoel@gmail.com>
2023-02-22 23:47:37 +0100

    made navbar text-bold

--- src/Components/LandingPageComponents/Nav.jsx
+++ src/Components/LandingPageComponents/Nav.jsx
@@ -6,11 +6,11 @@ const Nav = () => {
   return (
     <div className=" ">
       <div className="lg:block hidden">
-        <div className=" font-poppins font-bold w-full bg-[#87ACA3]/[0.4] 2xl:px-[10rem] xl:px-[8rem] lg:px-10  absolute top-40 py-3  z-40 rounded-b-lg flex justify-between items-center">
+        <div className=" font-poppins font-semibold w-full bg-[#87ACA3]/[0.4] 2xl:px-[10rem] xl:px-[8rem] lg:px-10  absolute top-40 py-3  z-40 rounded-b-lg flex justify-between items-center">
           <a href="">
             <img src={logo} alt="" className="object-contain w-[9rem]" />
           </a>
-          <span className="flex text-white w-[29rem] justify-between items-center font-light text-sm">
+          <span className="flex  text-white w-[29rem] justify-between items-center  text-sm">
             <a href="">How it works</a>
             <a href="">FAQs</a>
             <a href="">Login</a>
--- src/Components/LandingPageComponents/Steps.jsx
+++ src/Components/LandingPageComponents/Steps.jsx
@@ -5,8 +5,8 @@ const Steps = () => {
     <div className="px-2 sm:px-10  md:px-8  lg:px-10  xl:px-[10rem] mt-20 mb-64">
       <div className="float-right text-right text-[#2B7C85]">
         {" "}
-        <p className="text-xl">How It Works</p>
-        <p>Send money across borders in 4 easy steps</p>
+        <p className="text-3xl font-bold">How It Works</p>
+        <p className="mt-5 font-semibold">Send money across borders in 4 easy steps</p>
       </div>
     </div>
   );
