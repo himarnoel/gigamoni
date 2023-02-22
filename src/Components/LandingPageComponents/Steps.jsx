20e136210584d70fa0c8692a392eb7f75318504c
himarnoel <olaimarnoel@gmail.com>
2023-02-22 23:30:50 +0100

    added first text of steps.jsx

--- src/Components/LandingPageComponents/Steps.jsx
+++ src/Components/LandingPageComponents/Steps.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Steps = () => {
   return (
-    <div className="px-2 sm:px-10  md:px-8  lg:px-10  xl:px-[10rem] mt-20">
-      Steps
+    <div className="px-2 sm:px-10  md:px-8  lg:px-10  xl:px-[10rem] mt-20 mb-64">
+     <p>How It Works</p>
+     
     </div>
   );
 };
