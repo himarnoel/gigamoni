6d21aa5807e4c712925246bc4028bd838291f963
himarnoel <olaimarnoel@gmail.com>
2023-02-22 23:31:39 +0100

    added send text

--- src/Components/LandingPageComponents/Steps.jsx
+++ src/Components/LandingPageComponents/Steps.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Steps = () => {
   return (
     <div className="px-2 sm:px-10  md:px-8  lg:px-10  xl:px-[10rem] mt-20 mb-64">
-     <p>How It Works</p>
-     
+      <p>How It Works</p>
+      <p>Send money across borders in 4 easy steps</p>
     </div>
   );
 };
