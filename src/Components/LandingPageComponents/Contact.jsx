ad71f2fe1f193f680357cc7d9f31e8d2c960b9bc
himarnoel <olaimarnoel@gmail.com>
2023-02-24 19:05:23 +0100

    worked on the stack image

--- src/Components/LandingPageComponents/Contact.jsx
+++ src/Components/LandingPageComponents/Contact.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import img from "../../assets/contact/call.svg";
+import map from "../../assets/contact/map.svg";
+import call from "../../assets/contact/call.svg";
 const Contact = () => {
   return (
-    <div className="mt-20 text-center mb-[40rem]">
+    <div className="mt-20 text-center mb-[40rem]bg-[#87ACA3]  w-full pb-20  px-2 sm:px-10  md:px-8  lg:px-10  xl:px-[10rem]  py-[1rem] mb-[40rem]">
       <p className="text-[#2B7C85] text-sm text-[20px]">Contact Us</p>
       <p className="text-[#175873] text-3xl mt-4 font-semibold ">
         We would like to hear from you
@@ -11,7 +12,13 @@ const Contact = () => {
       <div className="w-full flex ">
         <div className="flex flex-col">
           <p>Our friendly 24/7 support team is here to help you</p>
-          <img src={img} alt="" />
+          <div className="relative mt-1">
+            <img src={map} className="object-cover" />
+            <img
+              src={call}
+              className="object-contain w-20 absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]"
+            />
+          </div>
         </div>
       </div>
     </div>
