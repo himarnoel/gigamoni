8a21fb0e7e3aa662dc5237ba1e79c00ee6b6984b
himarnoel <olaimarnoel@gmail.com>
2023-02-25 06:22:11 +0100

    made changes in name

--- src/Components/LandingPageComponents/Foot.jsx
+++ src/Components/LandingPageComponents/Foot.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Foot = () => {
-  return <div></div>;
+  return (
+    <div className="mt-20 text-center  w-full pb-20  px-2 sm:px-10  md:px-8  lg:px-10  xl:px-[10rem]  py-[1rem] ">
+      <div className="flex">
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Foot;
