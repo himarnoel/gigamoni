ceb954e8c992f972bda150d88549f74689829cf8
himarnoel <olaimarnoel@gmail.com>
2023-02-25 07:09:53 +0100

    done with md

--- src/Components/LandingPageComponents/Foot.jsx
+++ src/Components/LandingPageComponents/Foot.jsx
@@ -8,27 +8,26 @@ const Foot = () => {
     <div className="mt-20 text-center bg-[#004943] py-10  f items-start w-full   px-2 sm:px-10  md:px-8  lg:px-10  xl:px-[10rem]    ">
       <div className="flex items-center justify-between ">
         <div className=" flex flex-col ">
-          <img src={footlogo} alt="" className="object-contain w-1rem]"/>
-          
+          <img src={footlogo} alt="" className="object-contain w-[10rem]" />
         </div>
 
-        <div className="flex   text-white lg:w-[28rem] xl:w-[28rem] justify-between items-center  text-[0.7rem]">
+        <div className="flex   text-white md:w-[24rem] lg:w-[28rem] xl:w-[28rem] justify-between items-center md:text-[0.6rem]   lg:text-[0.7rem]">
           <a href="">FAQs</a>
           <a href="">Contact Us</a>
           <a href="">How it works</a>
           <a href="">Privacy Policy</a>
           <a href="">Terms and conditions</a>
         </div>
-        <div className="flex items-center justify-between text-white  w-[10rem] text-3xl">
+        <div className="flex items-center justify-between text-white md:w-[8rem] lg:w-[10rem] md:text-xl lg:text-3xl">
           <ImFacebook2 />
           <BsTwitter />
           <BsInstagram />
         </div>
       </div>
-      <span className="flex items-center ml-10 mt- ">
-            <p className=" text-[0.5rem] text-[#F8F8FF] mr-2">Powered By</p>
-            <img src={powerlogo} alt="" />
-          </span>
+      <span className="flex items-center ml-5 lg:ml-10 mt- ">
+        <p className=" text-[0.5rem] text-[#F8F8FF] mr-2">Powered By</p>
+        <img src={powerlogo} alt="" />
+      </span>
     </div>
   );
 };
