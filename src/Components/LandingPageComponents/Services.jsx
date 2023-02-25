19919495804a45c0a899c2d826183fb2b91376e6
himarnoel <olaimarnoel@gmail.com>
2023-02-25 11:36:13 +0100

    done with mobile for  services

--- src/Components/LandingPageComponents/Services.jsx
+++ src/Components/LandingPageComponents/Services.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import bg from "../../assets/Rectangle.png";
 const Services = () => {
   return (
-    <div className="bg-[#87ACA3]  w-full mt-20  pb-20  px-2 sm:px-10  md:px-8  lg:px-10  xl:px-[10rem]  py-[1rem]">
+    <div className="bg-[#87ACA3]  w-full mt-20  px-2 xss:px-4 xs:px-6  sm:px-10  md:px-8  lg:px-10  xl:px-[10rem]  py-[1rem]">
       <p className="text-center text-[#F8F8FF] text-3xl font-semibold mt-5 ">
         Explore our Services
       </p>
@@ -14,10 +14,10 @@ const Services = () => {
             alt=""
             className="object-cover w-full h-[20rem]  xs:h-[18rem] rounded-[16px] opacity-[0.1] md:rounded-[23.8886px] mix-blend-overlay "
           />
-          <p className=" text-lg md:text-sm  lg:text-lg md:ml-5 left-0 right-0 font-semibold absolute top-[1.3rem] xs:top-[1.2rem] text-center">
+          <p className=" text-lg md:text-sm  lg:text-lg md:ml-5 px-2 xss:px-4 xs:px-6  left-0 right-0 font-semibold absolute top-[1.3rem] xs:top-[1.1rem] text-center">
             Secure Funding for Large Purchase Orders
           </p>
-          <p className="text-xs font-normal mx-5 font-[#262626] absolute top-[5rem] xss:top-[6rem] xs:top-[5.5rem] md:top-[4.6rem] left-0 right-0 leading-[1.3rem] xss:leading-[1.6rem]  xs:leading-[1.5rem]   md:leading-[1.5rem] lg:leading-[2rem] ">
+          <p className="text-xs font-normal mx-5 font-[#262626] absolute top-[5rem] xss:top-[6rem] xs:top-[4.8rem] md:top-[4.6rem] left-0 right-0 leading-[1.3rem] xss:leading-[1.6rem]  xs:leading-[1.5rem]   md:leading-[1.5rem] lg:leading-[2rem] ">
             We aim to provide small businesses with a new way to access working
             capital, by allowing them to crowdfund their purchase orders and
             connect with buyers directly. This eliminates the need for
