eb664016d6135831c05f304d86ce9db30ef65548
himarnoel <olaimarnoel@gmail.com>
2023-02-26 16:31:09 +0100

    login rafce added

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./../Components/LandingPageComponents/Nav";
+
+const Login = () => {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+};
+
+export default Login;
