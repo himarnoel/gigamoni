2bcfb1dcc15f950b35a835cd99c17d495091894a
himarnoel <olaimarnoel@gmail.com>
2023-02-26 16:23:38 +0100

    crated path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import LandingPage from "./Pages/LandingPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
