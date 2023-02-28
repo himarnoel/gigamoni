bf815cba75d7a28b604535a2fcf1c31ba40b99f4
himarnoel <olaimarnoel@gmail.com>
2023-02-28 14:29:38 +0100

    check mail added

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import LandingPage from "./Pages/LandingPage";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Pages/Auth/Login";
 import Signup from "./Pages/Auth/Signup";
+import CheckMail from "./Pages/Auth/CheckMail";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/check" element={<CheckMail />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/Auth/CheckMail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CheckMail = () => {
+  return (
+    <div>CheckMail</div>
+  )
+}
+
+export default CheckMail
