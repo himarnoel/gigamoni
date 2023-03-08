879e7f02e6174e7976083b516028d1f524743323
himarnoel <olaimarnoel@gmail.com>
2023-03-08 09:12:50 +0100

    removed bug

--- src/Pages/Auth/RecoverPass.jsx
+++ src/Pages/Auth/RecoverPass.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import NavBar from "./../../Components/AppComponents/NavBar";
 import img1 from "../../assets/Vector.svg";
+import  RingLoader  from "react-spinners";
 const RecoverPass = () => {
   const [load, setload] = useState(false);
   const Resender = () => {
