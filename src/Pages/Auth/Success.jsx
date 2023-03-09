bc9149c855140f71b3ffb7416a5d78539994f730
himarnoel <olaimarnoel@gmail.com>
2023-03-09 07:33:12 +0100

    working on ti

--- src/Pages/Auth/Success.jsx
+++ src/Pages/Auth/Success.jsx
@@ -8,12 +8,12 @@ const Success = () => {
     <div>
       <div className="flex flex-col h-screen  font-poppins justify-between ">
         <NavBar />
-        <div className="bg flex-auto flex flex-col justify-between   ">
-          <div className="w-full px-2 xss:px-4 xs:px-6 xsm:px-[5rem] sm:px-0 sm:w-fit  mx-auto mxl:mt-[14rem]">
-            <h1 className="text-center text-[#F8F8FF] text-base sm:text-2xl lg:text-2xl xl:text-2xl mxl:text-3xl 2xl:text-3xl  sm:mt-8 lg:mt-[rem]  xl:mt-4 mt-5 mxl:mt-10 font-semibold ">
+        <div className="bg flex-auto sm:flex flex-col justify-center items-center   ">
+          <div className="w-full px-2 xss:px-4 xs:px-6 xsm:px-[5rem] sm:px-0 sm:w-fit  mx-auto mt-[8rem] mxl:mt-[-20rem] ">
+            <h1 className="text-center text-[#F8F8FF] text-base sm:text-2xl lg:text-2xl xl:text-2xl mxl:text-3xl 2xl:text-3xl  sm:mt-8 lg:mt-[rem]  xl:mt-[0rem] mxl:mt-[0rem] mt-5 font-semibold ">
               Password Reset Successfully
             </h1>
-            <div className="mx-auto bg-[#F8F8FF] w-full h-[87%] items-center   sm:w-[35rem] sm:h-[20rem]  md:w-[40rem] md:h-[25rem] lg:h-[19rem] lg:w-[30rem] mxl:w-[40rem] mxl:h-[25rem] md:mt-[5rem] lg:mt-[3rem]  rounded-[11.8392px] shade px-4 py-2 sm:px-10 sm:py-8 md:px-20 md:py-14 lg:px-10 lg:py-10 flex flex-col justify-around  xl:mt-5">
+            <div className="mx-auto bg-[#F8F8FF] w-full h-[20rem] items-center  mt-8  sm:w-[35rem] sm:h-[20rem] mxl:mt-[4rem] md:w-[40rem] md:h-[25rem] lg:h-[19rem] lg:w-[30rem] mxl:w-[40rem] mxl:h-[25rem] md:mt-[5rem] lg:mt-[3rem]  rounded-[11.8392px] shade px-4 py-2 sm:px-10 sm:py-8 md:px-20 md:py-14 lg:px-10 lg:py-10 flex flex-col justify-around  xl:mt-5">
               {" "}
               <span className="h-[4rem]  mx-auto  w-[4rem] xs:h-[5rem] xs:w-[5rem] md:h-[6rem] md:w-[6rem]   lg:h-[5rem] lg:w-[5rem]   flex justify-center items-center rounded-full bg-[#00913E]/[0.1] mt-3 md:mt-[2rem] lg:mt-[0rem]">
                 {" "}
@@ -23,9 +23,9 @@ const Success = () => {
                   className=" h-[2rem] object-contain md:h-[2.5rem] lg:h-[2rem]"
                 />
               </span>
-              <p className="text-sm text-center">
-                Your new password has been updated successfully, Login with the
-                new password to updated your account
+              <p className=" text-xs md:text-sm text-center leading-[2]">
+                Your new password has been updated successfully,
+              Login with the new password to updated your account
               </p>
               <button
                 onClick={() => navigate("/login")}
