979bab67bad317aa311bef9969ca680869999599
himarnoel <olaimarnoel@gmail.com>
2023-03-16 14:20:45 +0100

    done with half of  dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -28,14 +28,14 @@ const Dashboard = () => {
               <p className="text-[#175873] font-medium text-base ">
                 Recent Transactions
               </p>
-              <p className="text-[#009186] ml-[10rem] text-sm">Beneficiaries</p>
+              <p className="text-[#009186] ml-[5rem] text-sm">Beneficiaries</p>
             </span>
 
             <div className="flex items-center mt-8">
               <div className="relative ">
                 <input
                   type="text"
-                  className=" pl-3 pr-[15rem] py-3 text-sm placeholder:text-[#87ACA3] placeholder:text-xs rounded-[8px] border-[2px] focus:outline-none border-[#009186] focus:border-[#009186]  border-solid
+                  className=" pl-3 pr-[10rem] py-3 text-sm placeholder:text-[#87ACA3] placeholder:text-xs rounded-[8px] border-[2px] focus:outline-none border-[#009186] focus:border-[#009186]  border-solid
             "
                   placeholder="Search transcations"
                 />
@@ -57,18 +57,21 @@ const Dashboard = () => {
           </div>
           {/* for the secon part */}
           <div className="mt-10">
-            <div className="w-[29rem] h-[8.5rem] flex items-center justify-between rounded-3xl bg-[#87ACA3] pl-2">
-              <span className="w-[17rem]">
-                <p className="text-white text-sm ">
+            <div className="w-[29rem] h-[9.9rem] flex items-center justify-between rounded-3xl bg-[#87ACA3] pl-5">
+              <span className="w-[16rem]">
+                <p className="text-white text-sm font-medium ">
                   International transactions has never been easier
                 </p>
-                <button className="px-[2.4rem] py-[0.6rem] mt-4 text-sm rounded-lg text-white bg-[#009186] flex items-center">
-                  Account
+                <button className="px-[2rem] py-[0.6rem] mt-6 text-sm rounded-lg text-white bg-[#009186] flex items-center">
+                  Send Money
                   <RiArrowDownSLine className="text-xl ml-2" />
                 </button>
-               
               </span>
-              <img src={card} alt="" className="object-contain w-[10rem] mt-[-1.2rem] mr-[-0.2rem]"/>
+              <img
+                src={card}
+                alt=""
+                className="object-contain w-[12rem] mt-[-1.6rem] mr-[-0.3rem]"
+              />
             </div>
           </div>
         </div>
