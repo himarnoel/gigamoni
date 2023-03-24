49488e828215a51e4e7666d8ce12ccbaaf084b4a
himarnoel <olaimarnoel@gmail.com>
2023-03-24 22:00:34 +0100

    proof

--- src/Pages/Main_Pages/UploadProf.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UploadProf = () => {
-  return <div>UploadProf</div>;
-};
-
-export default UploadProf;
---
+++ src/Pages/Main_Pages/UploadProof.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UploadProof = () => {
+  return <div>UploadProof</div>;
+};
+
+export default UploadProof;
