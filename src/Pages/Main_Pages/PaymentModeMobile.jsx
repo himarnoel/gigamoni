d0ebe0b7146600a331ead5c6e1ca7c45c7f66da7
himarnoel <olaimarnoel@gmail.com>
2023-03-29 20:55:03 +0100

    make edit

--- src/Pages/Main_Pages/PaymentModeMobile.jsx
+++ src/Pages/Main_Pages/PaymentModeMobile.jsx
@@ -2,15 +2,104 @@ import React from "react";
 import NavBar from "./../../Components/AppComponents/NavBar";
 import img1 from "../../assets/overlayimage/one.svg";
 import img2 from "../../assets/overlayimage/vector.svg";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 const PaymentModeMobile = () => {
   let location = useLocation();
+  const payWithTransfer = () => {
+    console.log(token);
+    blockScroll();
+    setload(true);
+
+    axios
+      .post(
+        `${baseurl}/transactions/`,
+        {
+          beneficiary: false,
+          receiverName: location.state.receivername,
+          receiverEmail: location.state.emailAddress,
+          receiverPhone: location.state.phoneNumber,
+          receiverAcctName: location.state.accountName,
+          receiverAcctNo: location.state.accountNumber,
+          receiverBankName: location.state.bankName,
+          receiverBankAddress: location.state.bankAddress,
+          receiverIban: location.state.iban,
+          receiverSwiftCode: location.state.swiftCode,
+          receiverCountry: location.state.receivingCountry,
+          currencySent: location.state.sendingcurrency,
+          currencyReceived: location.state.receivingcurrency,
+          amountReceived: location.state.amountReceived,
+          description: location.state.tractionDescription,
+          paymentMethod: "payWithTransfer",
+        },
+        {
+          headers: {
+            Authorization: `Token ${localStorage.getItem("LoggedIntoken")}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        allowScroll();
+        setload(false);
+        navigate("/dashboard");
+      })
+      .catch((e) => {
+        setload(false);
+        console.log(e);
+        allowScroll();
+      });
+  };
+
+  const payWithCard = () => {
+    setload(true);
+
+    axios
+      .post(
+        `${baseurl}/transactions/`,
+        {
+          beneficiary: false,
+          receiverName: location.state.receivername,
+          receiverEmail: location.state.emailAddress,
+          receiverPhone: location.state.phoneNumber,
+          receiverAcctName: location.state.accountName,
+          receiverAcctNo: location.state.accountNumber,
+          receiverBankName: location.state.bankName,
+          receiverBankAddress: location.state.bankAddress,
+          receiverIban: location.state.iban,
+          receiverSwiftCode: location.state.swiftCode,
+          receiverCountry: location.state.receivingCountry,
+          currencySent: location.state.sendingcurrency,
+          currencyReceived: location.state.receivingcurrency,
+          amountReceived: location.state.amountReceived,
+          description: location.state.tractionDescription,
+
+          paymentMethod: "payWithCard",
+        },
+        {
+          headers: {
+            Authorization: `Token ${localStorage.getItem("LoggedIntoken")}`,
+          },
+        }
+      )
+      .then((res) => {
+        setload(false);
+        navigate("/dashboard");
+      })
+      .catch((e) => {
+        setload(false);
+        console.log(e);
+      });
+  };
   return (
     <div className="flex lg:hidden justify-between h-screen font-poppins">
       <NavBar />
       <div className="bg flex-auto ">
         <div className="w-full pt-12 flex justify-center items-center h-screen px-10 ">
           <div className="bg-[#F8F8FF] h-[25rem] sm:h-[50rem] md:h-[60rem] rounded-[11.8392px] text-xs mxl:text-sm w-full py-8 px-4 sm:px-14  font-semibold">
-            <p className="text-center text-base  sm:text-lg">Select Mode Of Payment</p>
+            <p className="text-center text-base  sm:text-lg">
+              Select Mode Of Payment
+            </p>
             <div className="flex pl-3 pb-3 mt-10 md:mt-20 mxl:mt-14 items-center border-b border-[#87ACA3] text-[#000000] font-medium">
               {" "}
               <img src={img2} alt="" className="sm:w-[2.5rem] object-contain" />
