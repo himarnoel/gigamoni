034146e66b02cde2040a31359b188ea618cc55a8
himarnoel <olaimarnoel@gmail.com>
2023-03-29 20:58:10 +0100

    pay

--- src/Pages/Main_Pages/PaymentModeMobile.jsx
+++ src/Pages/Main_Pages/PaymentModeMobile.jsx
@@ -4,12 +4,13 @@ import img1 from "../../assets/overlayimage/one.svg";
 import img2 from "../../assets/overlayimage/vector.svg";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
-import { RingLoader } from 'react-spinners';
+import { RingLoader } from "react-spinners";
+import { useState } from "react";
 const PaymentModeMobile = () => {
   let location = useLocation();
+  console.log(location.state);
+  const [load, setload] = useState(false);
   const payWithTransfer = () => {
-    console.log(token);
-    blockScroll();
     setload(true);
 
     axios
@@ -111,12 +112,18 @@ const PaymentModeMobile = () => {
             <p className="text-center text-base  sm:text-lg">
               Select Mode Of Payment
             </p>
-            <div className="flex pl-3 pb-3 mt-10 md:mt-20 mxl:mt-14 items-center border-b border-[#87ACA3] text-[#000000] font-medium">
+            <div
+              onClick={() => payWithTransfer()}
+              className="flex pl-3 pb-3 mt-10 md:mt-20 mxl:mt-14 items-center border-b border-[#87ACA3] text-[#000000] font-medium"
+            >
               {" "}
               <img src={img2} alt="" className="sm:w-[2.5rem] object-contain" />
               <p className="ml-4  sm:text-lg">Pay with Bank Transfer</p>
             </div>
-            <div className="flex pl-3 pb-3 mt-10 md:mt-20 mxl:mt-12 items-center border-b border-[#87ACA3] text-[#000000] font-medium">
+            <div
+              onClick={() => payWithCard()}
+              className="flex pl-3 pb-3 mt-10 md:mt-20 mxl:mt-12 items-center border-b border-[#87ACA3] text-[#000000] font-medium"
+            >
               {" "}
               <img src={img1} alt="" className="sm:w-[2.5rem] object-contain" />
               <p className="ml-4  sm:text-lg">Pay with debit card</p>
