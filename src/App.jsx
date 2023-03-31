f2c1fe91e2ed6f96b62d698d817b906d093e9319
himarnoel <olaimarnoel@gmail.com>
2023-03-31 13:26:57 +0100

    protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import LandingPage from "./Pages/LandingPage";
 import { Navigate, Route, Routes } from "react-router-dom";
@@ -29,21 +29,28 @@ import PaymentModeMobile from "./Pages/Main_Pages/PaymentModeMobile";
 
 function App() {
   const [count, setCount] = useState(0);
+  const token = localStorage.getItem("LoggedIntoken");
 
   return (
     <div>
       <ToastContainer position="top-center" autoClose={2000} />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/login"
+          element={token ? <Navigate to="/dashboard" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={token ? <Navigate to="/dashboard" /> : <Signup />}
+        />
         <Route path="/check" element={<CheckMail />} />
         <Route path="/mobile" element={<SetMobile />} />
         <Route path="/verify" element={<VerifyMail />} />
         <Route path="/forgot" element={<Forgot />} />
         <Route path="/recover" element={<RecoverPass />} />
         <Route path="/newpassword" element={<NewPassword />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+
         <Route path="/success" element={<Success />} />
         <Route path="/send" element={<Send />} />
         <Route path="/less" element={<LessThan />} />
@@ -51,9 +58,22 @@ function App() {
         <Route path="/beneficiary" element={<BeneficiaryMobile />} />
         <Route path="/pending" element={<Pending />} />
         <Route path="/summary" element={<OrderSummary />} />
-        <Route path="/progress" element={<InProgress />} />
-        <Route path="/upload" element={<UploadProof />} />
-        <Route path="/pay" element={<PaymentModeMobile />} />
+        <Route
+          path="/dashboard"
+          element={token ? <Dashboard /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/progress"
+          element={token ? <InProgress /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/upload"
+          element={token ? <UploadProof /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/pay"
+          element={token ? <PaymentModeMobile /> : <Navigate to="/" />}
+        />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
