2b9c4b0f98064122edfa1839dd9e71af4179d7ed
himarnoel <olaimarnoel@gmail.com>
2023-04-03 17:23:44 +0100

    working on responsive upload

--- src/Pages/Main_Pages/UploadProof.jsx
+++ src/Pages/Main_Pages/UploadProof.jsx
@@ -96,11 +96,11 @@ const UploadProof = () => {
       >
         <NavBar class="top-0 fixed z-[20]" />
         <div className="mt-[6rem] flex justify-between mxl:pt-32 2xl:px-[10rem] xl:px-[5rem] lg:px-10  px-2 xss:px-4 xs:px-6 sm:px-10 md:px-8">
-          <button className=" text-sm px-[4rem] py-[0.7rem]  lg:px-[4rem] lg:py-[0.7rem] rounded-lg bg-[#87ACA3]">
+          <button className=" text-sm px-[3rem] py-[0.7rem]  lg:px-[4rem] lg:py-[0.7rem] rounded-lg bg-[#87ACA3] ">
             Back
           </button>
-          <div className="flex items-center font-semibold text-sm">
-            <p className="mr-20 text-[#009186]  cursor-pointer">
+          <div className="flex items-center justify-between font-semibold text-sm">
+            <p className=" mr-3sm:mr-20 text-[#009186] text-xs  sm:text-base cursor-pointer hidden lg:flex">
               Upload proof of payment
             </p>
             <div
@@ -114,14 +114,19 @@ const UploadProof = () => {
               />
               <p className="font-semibold mxl:text-xl">Notifications</p>
             </div>
+            <img
+              src={bell}
+              alt=""
+              className="object-contain w-[1.6rem] sm:w-[2.3rem] md:w-[2.5rem] lg:hidden "
+            />
           </div>
         </div>
-        <p className="text-[1.5rem] xl:text-[1.7rem] font-semibold text-center mt-6">
+        <p className="text-xl sm:text-[1.5rem] xl:text-[1.7rem] font-semibold text-center mt-8">
           Upload Proof of payment
         </p>
         <form
           onSubmit={formik.handleSubmit}
-          className="w-full px-10 lg:px-[10rem] xl:px-[19rem] mt-10 "
+          className="w-full px-10 lg:px-[10rem] xl:px-[19rem] mt-8 sm:mt-10 "
         >
           <div
             onClick={() => handleClick()}
@@ -178,7 +183,7 @@ const UploadProof = () => {
               id="transactionID"
               type="text"
               placeholder="Enter transaction ID"
-              className="border-2 border-[#87ACA3] placeholder:text-[#87ACA3] text-xs w-[20rem] px-4 py-3 mt-8 rounded-lg focus:outline-none focus:ring-0 focus:border-[#87ACA3]"
+              className="border-2 border-[#87ACA3] placeholder:text-[#87ACA3] text-xs w-[17rem] sm:w-[20rem] px-4 py-4 ml-[-1rem] sm:py-3 mt-8 rounded-lg focus:outline-none focus:ring-0 focus:border-[#87ACA3]"
               onChange={formik.handleChange}
               value={formik.values.transactionID}
             />
@@ -207,7 +212,7 @@ const UploadProof = () => {
             ) : (
               ""
             )}
-          
+
             <button
               type="submit"
               className={
