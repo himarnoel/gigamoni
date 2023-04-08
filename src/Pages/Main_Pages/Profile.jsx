aa26a289eaa66f10c7d50488690b6b75bae00162
himarnoel <olaimarnoel@gmail.com>
2023-04-08 22:53:30 +0100

    changed to new password

--- src/Pages/Main_Pages/Profile.jsx
+++ src/Pages/Main_Pages/Profile.jsx
@@ -261,33 +261,33 @@ const Profile = () => {
           <div className="relative z-0 mt-0">
             <input
               type="text"
-              id="iban"
+              id="newpassword"
               className={
-                changepasswordformik.errors.iban &&
-                changepasswordformik.touched.iban
+                changepasswordformik.errors.newpassword &&
+                changepasswordformik.touched.newpassword
                   ? "block font-poppins  w-full pl-8 pb-1 pt-3 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-[1.5px]  border-red-500 appearance-none   focus:outline-none focus:ring-0 focus:border-[#009186] peer"
                   : "block font-poppins  w-full pl-8 pb-1 pt-3 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-[1.5px] border-[#262626] appearance-none   focus:outline-none focus:ring-0 focus:border-[#009186] peer"
               }
               placeholder=" "
               onChange={changepasswordformik.handleChange}
-              value={changepasswordformik.values.iban}
+              value={changepasswordformik.values.newpassword}
               onBlur={changepasswordformik.handleBlur}
             />
             <label
               for="name"
               className={
-                changepasswordformik.errors.iban &&
-                changepasswordformik.touched.iban
+                changepasswordformik.errors.newpassword &&
+                changepasswordformik.touched.newpassword
                   ? "absolute text-xs mxl:text-sm font-poppins text-red-500  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100  peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   : "absolute text-xs mxl:text-sm font-poppins text-[#262626]  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100   peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               }
             >
               New Password
             </label>
-            {changepasswordformik.errors.iban &&
-            changepasswordformik.touched.iban ? (
+            {changepasswordformik.errors.newpassword &&
+            changepasswordformik.touched.newpassword ? (
               <p className="text-red-500 text-xs font-poppins">
-                {changepasswordformik.errors.iban}
+                {changepasswordformik.errors.newpassword}
               </p>
             ) : (
               ""
