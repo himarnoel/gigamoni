86e620f22130ba47aa748cd70f00dcae3a84a08f
himarnoel <olaimarnoel@gmail.com>
2023-04-08 22:48:10 +0100

    corrected for phone number

--- src/Pages/Main_Pages/Profile.jsx
+++ src/Pages/Main_Pages/Profile.jsx
@@ -124,30 +124,30 @@ const Profile = () => {
           <div className="relative z-0 mt-0">
             <input
               type="text"
-              id="iban"
+              id="phoneNumber"
               className={
-                 editprofileformik.errors.iban &&  editprofileformik.touched.iban
+                 editprofileformik.errors.phoneNumber &&  editprofileformik.touched.phoneNumber
                   ? "block font-poppins  w-full pl-8 pb-1 pt-3 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-[1.5px]  border-red-500 appearance-none   focus:outline-none focus:ring-0 focus:border-[#009186] peer"
                   : "block font-poppins  w-full pl-8 pb-1 pt-3 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-[1.5px] border-[#262626] appearance-none   focus:outline-none focus:ring-0 focus:border-[#009186] peer"
               }
               placeholder=" "
               onChange={ editprofileformik.handleChange}
-              value={ editprofileformik.values.iban}
+              value={ editprofileformik.values.phoneNumber}
               onBlur={ editprofileformik.handleBlur}
             />
             <label
               for="name"
               className={
-                 editprofileformik.errors.iban &&  editprofileformik.touched.iban
+                 editprofileformik.errors.phoneNumber &&  editprofileformik.touched.phoneNumber
                   ? "absolute text-xs mxl:text-sm font-poppins text-red-500  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100  peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   : "absolute text-xs mxl:text-sm font-poppins text-[#262626]  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100   peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               }
             >
               Phone number
             </label>
-            { editprofileformik.errors.iban &&  editprofileformik.touched.iban ? (
+            { editprofileformik.errors.phoneNumber &&  editprofileformik.touched.phoneNumber ? (
               <p className="text-red-500 text-xs font-poppins">
-                { editprofileformik.errors.iban}
+                { editprofileformik.errors.phoneNumber}
               </p>
             ) : (
               ""
