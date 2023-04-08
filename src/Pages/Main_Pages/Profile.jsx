318b7bafa895f8f37648485c9c2efad8ea3ba006
himarnoel <olaimarnoel@gmail.com>
2023-04-08 22:48:58 +0100

    address added

--- src/Pages/Main_Pages/Profile.jsx
+++ src/Pages/Main_Pages/Profile.jsx
@@ -158,28 +158,28 @@ const Profile = () => {
               type="text"
               id="address"
               className={
-                 editprofileformik.errors.address &&  editprofileformik.touched.swiftCode
+                 editprofileformik.errors.address &&  editprofileformik.touched.address
                   ? "block font-poppins  w-full pl-8 pb-1 pt-3 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-[1.5px]  border-red-500 appearance-none   focus:outline-none focus:ring-0 focus:border-[#009186] peer"
                   : "block font-poppins  w-full pl-8 pb-1 pt-3 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-[1.5px] border-[#262626] appearance-none   focus:outline-none focus:ring-0 focus:border-[#009186] peer"
               }
               placeholder=" "
               onChange={ editprofileformik.handleChange}
-              value={ editprofileformik.values.swiftCode}
+              value={ editprofileformik.values.address}
               onBlur={ editprofileformik.handleBlur}
             />
             <label
               for="name"
               className={
-                 editprofileformik.errors.swiftCode &&  editprofileformik.touched.swiftCode
+                 editprofileformik.errors.address &&  editprofileformik.touched.address
                   ? "absolute text-xs mxl:text-sm font-poppins text-red-500  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100  peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   : "absolute text-xs mxl:text-sm font-poppins text-[#262626]  duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#009186] peer-placeholder-shown:scale-100   peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               }
             >
               Address
             </label>
-            { editprofileformik.errors.swiftCode &&  editprofileformik.touched.swiftCode ? (
+            { editprofileformik.errors.address &&  editprofileformik.touched.address ? (
               <p className="text-red-500 text-xs font-poppins">
-                { editprofileformik.errors.swiftCode}
+                { editprofileformik.errors.address}
               </p>
             ) : (
               ""
