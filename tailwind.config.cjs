a3efc92b35f4144666142ac5008b083eee4dbe2c
himarnoel <olaimarnoel@gmail.com>
2023-04-08 13:30:38 +0100

    added a new package for checkbox

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-   
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
       xss: "300px",
@@ -18,5 +14,5 @@ module.exports = {
       poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
