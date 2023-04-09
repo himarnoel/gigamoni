5f34aaeccea91a5740efcb1ded55968d446caa00
himarnoel <olaimarnoel@gmail.com>
2023-04-09 14:57:03 +0100

    remove duplicate

--- src/Pages/Main_Pages/Pending.jsx
+++ src/Pages/Main_Pages/Pending.jsx
@@ -84,26 +84,8 @@ const Pending = () => {
 
       <NavBar class="fixed top-0 z-[2]" />
 
-      <div className="flex justify-between items-center mt-28 px-2 xss:px-4 xs:px-6  sm:mt-26  md:mt-32  lg:mt-20 lg:hidden  mxl:mt-10">
-        <button className=" text-sm px-[4rem] py-[0.7rem]  lg:px-[4rem] lg:py-[0.7rem] rounded-lg bg-[#87ACA3]">
-          Back
-        </button>
-        <div
-          onClick={() => alert("Notification")}
-          className=" text-[#009186] items-center cursor-pointer flex "
-        >
-          <img
-            src={bell}
-            alt=""
-            className="object-contain w-[1.6rem] mxl:w-[2rem] mr-3"
-          />
-          <p className="font-semibold mxl:text-xl hidden lg:block ">
-            Notifications
-          </p>
-        </div>
-      </div>
-      <div className=" 2xl:px-[10rem] xl:px-[5rem]  xss:pl-4  xss:pr-10   xs:pl-6 xs:pr-12 sm:pl-10 sm:pr-28 md:pl-8 md:pr-28 lg:px-10 w-full mt-4  lg:mt-20  mxl:pt-32 mb-20 sm:mb-32">
-        <div className=" justify-between items-center  mxl:mt-10 hidden lg:flex">
+      <div className=" 2xl:px-[10rem] xl:px-[5rem]  xss:pl-4  xss:pr-10 mt-28   xs:pl-6 xs:pr-12 sm:pl-10 sm:pr-28 md:pl-8 md:pr-28 lg:px-10 w-full  lg:mt-20  mxl:pt-20 mb-20 sm:mb-32">
+        <div className=" justify-between items-center  mxl:mt-10 flex">
           <button className=" text-sm px-[4rem] py-[0.7rem]  lg:px-[4rem] lg:py-[0.7rem] rounded-lg bg-[#87ACA3]">
             Back
           </button>
