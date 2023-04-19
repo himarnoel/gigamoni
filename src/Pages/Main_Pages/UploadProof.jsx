4bd21a08d2b1c537758eb3bdedc52d8efa8ab03a
himarnoel <olaimarnoel@gmail.com>
2023-04-19 13:37:36 +0100

    upload

--- src/Pages/Main_Pages/UploadProof.jsx
+++ src/Pages/Main_Pages/UploadProof.jsx
@@ -159,7 +159,10 @@ const UploadProof = () => {
       >
         <NavBar class="top-0 fixed z-[20]" />
         <div className="mt-[6rem] flex justify-between mxl:pt-32 2xl:px-[10rem] xl:px-[5rem] lg:px-10  px-2 xss:px-4 xs:px-6 sm:px-10 md:px-8">
-          <button className=" text-sm px-[3rem] py-[0.7rem]  lg:px-[4rem] lg:py-[0.7rem] rounded-lg bg-[#87ACA3] ">
+          <button
+            onClick={() => navigate("/dashboard")}
+            className=" text-sm px-[3rem] py-[0.7rem]  lg:px-[4rem] lg:py-[0.7rem] rounded-lg bg-[#87ACA3] "
+          >
             Back
           </button>
           <div className="flex items-center justify-between font-semibold text-sm">
