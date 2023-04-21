5c077e4b7a582fef599e7f1f0186252f0f32da5a
himarnoel <olaimarnoel@gmail.com>
2023-04-21 11:26:03 +0100

    working on loading

--- src/Pages/Main_Pages/BeneficiaryMobile.jsx
+++ src/Pages/Main_Pages/BeneficiaryMobile.jsx
@@ -1,19 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import DashNav from "../../Components/DashBoardComponents/DashNav";
 import bell from "../../assets/bell.svg";
 import { BiTransfer } from "react-icons/bi";
 import caller from "../../assets/Dashboard/caller.svg";
+import axios from "axios";
+import { baseurl } from "../../Service/validate_and_api";
+import { toast } from "react-toastify";
+import { RingLoader } from "react-spinners";
 const BeneficiaryMobile = () => {
+  const [data, setdata] = useState([]);
+  const [load, setload] = useState(false);
+  const safeDocument = typeof document !== "undefined" ? document : {};
+  const { body } = safeDocument;
   useEffect(() => {
     window.scroll({ top: 0, left: 0 });
     const val = localStorage.getItem("LoggedIntoken");
     if (!val) {
       navigate("/login");
+    } else {
+      setload(true);
+      axios
+        .get(`${baseurl}/transactions/beneficiary/`, {
+          headers: {
+            Authorization: `Token ${localStorage.getItem("LoggedIntoken")}`,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setload(false);
+          setdata(res.data);
+        })
+        .catch((e) => {
+          console.log(e);
+          setload(false);
+          toast.error("An error occurred");
+        });
     }
   }, []);
   return (
     <div className="font-poppins ">
-      {" "}
+      <div
+        className={
+          load
+            ? "absolute top-0   bg-[#262626]/[0.8]   z-[90] h-screen w-full flex  justify-center items-center text-3xl"
+            : "hidden"
+        }
+      >
+        <RingLoader color="#009186" size={90} />
+      </div>{" "}
       <DashNav class="fixed top-0 w-full z-[30] " />
       <div className="px-2 xss:px-4 xs:px-6 pt-20">
         <div className="w-full lg:hidden flex justify-between items-center mt-10">
@@ -33,16 +67,18 @@ const BeneficiaryMobile = () => {
           />
         </div>
         <p className="text-center mt-10">Beneficiaries</p>
-        <div className="mt-4 bg-[#DAF2F1] w-full h-[20rem] rounded-lg px-4  overflow-y-auto ">
-          <div className="w-full border-2 border-[#009186] border-solid min-h-[5rem] rounded-lg mt-5 px-2 pt-5 bg-white  pb-3">
-            <p className="text-xs font-semibold text-[#175873]">
-              Beneficiary Name
-            </p>
-            <div className="flex text-[#262626] text-xs mt-5">
-              <p className="mr-5">JPMorgan Chase Bank</p>
-              <p className="">12345678901234 </p>
+        <div className="mt-4 bg-[#DAF2F1] w-full h-[25rem] rounded-lg px-4  overflow-y-auto ">
+          {data.map((item, i) => (
+            <div className="w-full border-2 border-[#009186] border-solid min-h-[5rem] rounded-lg mt-5 px-2 pt-5 bg-white  pb-3">
+              <p className="text-xs font-semibold text-[#175873]">
+                {item.fullName}
+              </p>
+              <div className="flex text-[#262626] text-xs mt-5">
+                <p className="mr-5">{item.bankName}</p>
+                <p className="">{item.id}</p>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
