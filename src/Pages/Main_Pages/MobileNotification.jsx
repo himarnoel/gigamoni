5e67549f6ce05aa4cd21e5c5db75b2c4dc44297c
himarnoel <olaimarnoel@gmail.com>
2023-05-15 12:49:22 +0100

    mobile

--- src/Pages/Main_Pages/MobileNotification.jsx
+++ src/Pages/Main_Pages/MobileNotification.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import NavBar from './../../Components/AppComponents/NavBar';
+import React from "react";
+import NavBar from "./../../Components/AppComponents/NavBar";
 
 const MobileNotification = () => {
   return (
     <div>
-    <NavBar />
+      <NavBar />
+      <button className="bg-[#87ACA3]  text-[#262626] h-10 px-10  rounded-lg">
+        Back
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default MobileNotification
+export default MobileNotification;
