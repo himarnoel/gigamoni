667c87fd109cf9be5aa24b5ed6cfc159387af4fa
himarnoel <olaimarnoel@gmail.com>
2023-05-15 13:21:56 +0100

    added here

--- src/Pages/Main_Pages/BankTransfer.jsx
+++ src/Pages/Main_Pages/BankTransfer.jsx
@@ -89,9 +89,10 @@ const BankTransfer = () => {
             className=" text-[#009186] items-center cursor-pointer hidden lg:flex  "
           >
             <img
+              onClick={() => navigate("/mobileviewofnotification")}
               src={bell}
               alt=""
-              className="object-contain w-[1.6rem] mxl:w-[2rem] mr-3"
+              className="object-contain w-[1.6rem] sm:w-[2.3rem] md:w-[2.5rem] "
             />
             <p className="font-semibold mxl:text-xl">Notifications</p>
           </div>
